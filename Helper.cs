0860e02dcf88d8d4bee373a7d9bec2d2aa3e72ee
paulrwd85@gmail.com <paulrwd85@gmail.com>
2021-03-22 14:50:37 +0300

    2203_1

---
+++ Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YahFin.Models;
+using YahFin.QuotePrice_Classes;
+using YahFin.QuoteChart_Classes;
+
+
+namespace YahFin
+{
+    public class Helper
+    {
+        public static void JapCandleInstall(ref JapCandle jc1, QuoteChart getchart1, int v)
+        {
+            jc1.open = getchart1.Chart.Result[0].Indicators.Quote[0].Open[v];
+            jc1.close = getchart1.Chart.Result[0].Indicators.Quote[0].Close[v];
+            jc1.low = getchart1.Chart.Result[0].Indicators.Quote[0].Low[v];
+            jc1.high = getchart1.Chart.Result[0].Indicators.Quote[0].High[v];
+            jc1.adjclose = getchart1.Chart.Result[0].Indicators.Adjclose[0].adjclose[v];
+            jc1.volume = getchart1.Chart.Result[0].Indicators.Quote[0].Volume[v];
+        }
+    }
+}
--- JapCandleTypizator.cs
+++ JapCandleTypizator.cs
@@ -9,13 +9,30 @@ namespace YahFin
     {
         public static string GetType(JapCandle jc)
         {
+            double half_percent = jc.close / 500;
+
             if(jc.open > jc.close)
             {
+                if(jc.open - jc.close < half_percent)
+                {
+                    return "Красная дожи";
+                }
+
                 return "Красная";
+
             }
             else if(jc.open < jc.close)
             {
+                if (jc.close - jc.open < half_percent)
+                {
+                    return "Зеленая дожи";
+                }
+
                 return "Зеленая";
+            }
+            else if (jc.open == jc.close)
+            {
+                return "Дожи";
             }
 
             return "";
